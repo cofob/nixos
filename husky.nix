68348fa598a0558f5c9135c6a9de28fd2fa52741
Egor Ternovoy <cofob@riseup.net>
2022-12-03 21:28:41 +0700

    Enable TPM

--- husky.nix
+++ husky.nix
@@ -19,6 +19,11 @@
     ];
   };
 
+  security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+  users.users.cofob.extraGroups = [ "tss" ]; # tss group has access to TPM devices
+
   age.secrets.sync-husky-cert.file = ./secrets/syncthing/husky-cert.age;
   age.secrets.sync-husky-key.file = ./secrets/syncthing/husky-key.age;
   age.secrets.sync-husky-cert.owner = "cofob";
