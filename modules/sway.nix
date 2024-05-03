4e3cab74c5a0c0c6f3b0773a4a8369eca1224d52
cofob <cofob@riseup.net>
2024-05-04 03:19:57 +0400

    Fix brightness issues

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.xserver.enable = true;
@@ -8,8 +8,11 @@
   security.polkit.enable = true;
 
   programs.light.enable = true;
-
   users.users.cofob.extraGroups = ["video"];
+  # https://wiki.archlinux.org/title/backlight#ACPI
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video $sys$devpath/brightness", RUN+="${pkgs.coreutils}/bin/chmod g+w $sys$devpath/brightness"
+  '';
 
   programs.sway = {
     enable = true;
