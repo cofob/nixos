1b143b808e72f3b616c6e33cf063f60c8c5f4052
cofob <cofob@riseup.net>
2024-05-05 03:40:50 +0400

    Add fmt script

--- machines/husky/home.nix
+++ machines/husky/home.nix
@@ -1,6 +1,10 @@
 { self, ... }:
 
 {
-  imports =
-    [ "${self}/home/common.nix" "${self}/home/gui.nix" "${self}/home/dev.nix" "${self}/home/games.nix" ];
+  imports = [
+    "${self}/home/common.nix"
+    "${self}/home/gui.nix"
+    "${self}/home/dev.nix"
+    "${self}/home/games.nix"
+  ];
 }
