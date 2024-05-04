1e8899d12156d3f804b9a9987629fb5421583dd2
cofob <cofob@riseup.net>
2024-05-05 03:03:06 +0400

    Enable games for husky

--- machines/husky/home.nix
+++ machines/husky/home.nix
@@ -2,5 +2,5 @@
 
 {
   imports =
-    [ "${self}/home/common.nix" "${self}/home/gui.nix" "${self}/home/dev.nix" ];
+    [ "${self}/home/common.nix" "${self}/home/gui.nix" "${self}/home/dev.nix" "${self}/home/games.nix" ];
 }
