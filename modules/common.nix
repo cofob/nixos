79ed0e854209dc329d7aa24c96fab039178460cb
cofob <cofob@riseup.net>
2024-05-04 04:03:43 +0400

    Revert "Update upgrade-system script"
    This reverts commit 6255d667cf4616ba0c2db6a24e696110a482e4ec.

--- modules/common.nix
+++ modules/common.nix
@@ -47,7 +47,7 @@
   environment.systemPackages = with pkgs;
     let
       upgrade-system = pkgs.writeScriptBin "upgrade-system" ''
-        ${pkgs.nh}/bin/nh os switch github:cofob/nixos
+        sudo rm -rf /root/.cache && sudo nixos-rebuild switch --flake "github:cofob/nixos.git?ref=master"
       '';
     in [
       upgrade-system
