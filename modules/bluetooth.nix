6cceabb49427120fe033c83073216b544f43210e
cofob <cofob@riseup.net>
2024-05-13 01:56:03 +0400

    Update bluetooth config

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -3,7 +3,12 @@
 {
   hardware.bluetooth = {
     enable = true;
-    settings = { General = { Enable = "Source,Sink,Media,Socket"; }; };
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Disable = "Headset";
+      };
+    };
   };
   services.blueman.enable = true;
 }
