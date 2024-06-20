c104f2dc5e507f939ee8700f3496bccf38268ee8
cofob <cofob@riseup.net>
2024-06-20 14:02:32 +0400

    Remove argv check

--- packages/py3env.nix
+++ packages/py3env.nix
@@ -4,9 +4,6 @@ writers.writePython3Bin "py3env" { libraries = [ ]; } ''
   import sys
   import os
 
-  if len(sys.argv) == 1:
-      raise RuntimeError("at least 1 package is required")
-
   args = sys.argv[1:]
   shell_args = [f"python3Packages.{arg}" for arg in args]
 
