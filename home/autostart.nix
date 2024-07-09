0174478dd234675cd7ea6c550eaa7b3669359e8e
cofob <cofob@riseup.net>
2024-07-09 15:39:06 +0400

    Fix

--- home/autostart.nix
+++ home/autostart.nix
@@ -2,7 +2,7 @@
 
 {
   # Enable MegaSync at startup
-  xdg.configFile = lib.mkIf pkgs.system == "x86_64-linux" {
+  xdg.configFile = lib.mkIf (pkgs.system == "x86_64-linux") {
     "autostart/megasync.desktop".source =
       "${pkgs.megasync}/share/applications/megasync.desktop";
   };
