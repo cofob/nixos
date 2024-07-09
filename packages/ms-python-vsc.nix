c9f1be9444a21e9e591ade3cbdaba66729f5fa72
cofob <cofob@riseup.net>
2024-07-09 15:34:17 +0400

    Fix

--- packages/ms-python-vsc.nix
+++ packages/ms-python-vsc.nix
@@ -1,6 +1,6 @@
-{ ms-python, ... }:
+{ vscode-extensions, ... }:
 
-ms-python.python.overrideAttrs {
+vscode-extensions.ms-python.python.overrideAttrs {
   meta.platforms =
     [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
 }
--- packages/top-level.nix
+++ packages/top-level.nix
@@ -4,5 +4,5 @@
   yubikey-waybar = callPackage ./yubikey-waybar { };
   py3env = callPackage ./py3env.nix { };
 
-  ms-python.python = callPackage ./ms-python-vsc.nix { };
+  vscode-extensions.ms-python.python = callPackage ./ms-python-vsc.nix { };
 }
