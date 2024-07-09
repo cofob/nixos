86ed799bf526dbfca6ff8b78831f8d0ca486762f
cofob <cofob@riseup.net>
2024-07-09 15:36:38 +0400

    Revert

--- packages/ms-python-vsc.nix
+++
@@ -1,6 +0,0 @@
-{ vscode-extensions, ... }:
-
-vscode-extensions.ms-python.python.overrideAttrs {
-  meta.platforms =
-    [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-}
--- packages/top-level.nix
+++ packages/top-level.nix
@@ -3,6 +3,4 @@
 {
   yubikey-waybar = callPackage ./yubikey-waybar { };
   py3env = callPackage ./py3env.nix { };
-
-  vscode-extensions.ms-python.python = callPackage ./ms-python-vsc.nix { };
 }
