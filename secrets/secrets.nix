826829f3ec6471a9ca1222073accb2bd0a21b623
cofob <cofob@riseup.net>
2024-07-11 01:03:17 +0400

    Update secret keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,18 @@
 let
-  cofob =
+  cofob-twinkpad =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzB+g6qthKy95lG3UxnikrHCaVZ9O9njEqzdCIIfXxL cofob@twinkpad";
+  cofob-yubi =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJsSKOtKRM9+bvCs6iioOrcayMdsdwaQN/lJAQJkXE+w";
-  users = [ cofob ];
+  cofob = [ cofob-twinkpad cofob-yubi ];
+  users = cofob;
 
-  husky =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFrYiKbERa6EiZHSBBQvjME2qAMowHimuXb+KOUS8Rn";
-  twinkpad =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcgBUw3N8+F9qvcMd6GWHYEtmquY5HsIGOCsS9pcCsP";
-  systems = [ husky twinkpad ];
+  husky = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFrYiKbERa6EiZHSBBQvjME2qAMowHimuXb+KOUS8Rn"
+  ];
+  twinkpad = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcgBUw3N8+F9qvcMd6GWHYEtmquY5HsIGOCsS9pcCsP"
+  ];
+  systems = husky ++ twinkpad;
 
   all = users ++ systems;
 in { "./passwords/cofob.age".publicKeys = all; }
